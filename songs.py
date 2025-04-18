6507128c3bd3fa2e3b9d934ad0b7920734246a85
Pratham Bhandary <prathamb1501@gmail.com>
2025-04-18 11:58:34 +0530

    🎃

--- songs.py
+++ songs.py
@@ -3,4 +3,10 @@ import os
 def get_songs(app):
     music_folder = os.path.join(app.static_folder, 'music')
     # List all mp3 files in the folder
-    return [file for file in os.listdir(music_folder) if file.endswith('.mp3')]
+    x = [file for file in os.listdir(music_folder) if file.endswith('.mp3')]
+    new_list = []
+    i = 0
+    for song in x:
+        sang = song.replace(".mp3", ".jpg")
+        new_list.append([song, "Artist", sang])
+    return new_list
