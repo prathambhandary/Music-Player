f66de16a8f35a135ab90fa844756a36d838ce854
Pratham Bhandary <prathamb1501@gmail.com>
2025-04-27 15:57:09 +0530

    🛌

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 def init_db():
-    conn = sqlite3.connect('artist.db')
+    conn = sqlite3.connect(r'C:\Users\HP\OneDrive\Desktop\Pratham\0day\Code Challenge\Music-Player\artist.db')
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS songs (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -11,14 +11,14 @@ def init_db():
     conn.close()
 
 def add_song(title, artist):
-    conn = sqlite3.connect('artist.db')
+    conn = sqlite3.connect(r'C:\Users\HP\OneDrive\Desktop\Pratham\0day\Code Challenge\Music-Player\artist.db')
     c = conn.cursor()
     c.execute("INSERT INTO songs (title, artist) VALUES (?, ?)", (title, artist))
     conn.commit()
     conn.close()
 
 def retrieve_artist(song_name):
-    conn = sqlite3.connect('artist.db')
+    conn = sqlite3.connect(r'C:\Users\HP\OneDrive\Desktop\Pratham\0day\Code Challenge\Music-Player\artist.db')
     cursor = conn.cursor()
     cursor.execute("SELECT artist FROM songs WHERE title = ?", (song_name,))
     result = cursor.fetchone()
